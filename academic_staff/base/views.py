e665462d859b7fbd80eefa7dd1619012bb8584d1
Frolov-Andrey2405 <senior.slender2015@gmail.com>
2023-02-11 21:59:28 +0200

    Flash Messages

--- academic_staff/base/views.py
+++ academic_staff/base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.db.models import Q
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -75,11 +76,18 @@ def login_page(request):
         username = request.POST.get('username')
         password = request.POST.get('password')
 
-
         try:
             user = User.objects.get(username=username)
-        except Room.DoesNotExist:
-            None
+        except User.DoesNotExist:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid credentials')
 
     context = {}
     return render(request, 'base/login_register.html', context)
